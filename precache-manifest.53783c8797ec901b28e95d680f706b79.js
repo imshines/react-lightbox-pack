ad926606b668ccc0669bd936e84f965dec62a40f
Mohan Kumar <mohan@managementseye.com>
2021-07-10 12:00:47 +0530

    Updates

---
+++ precache-manifest.53783c8797ec901b28e95d680f706b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00d2e0a5a74f546c72aace27b44a956",
+    "url": "./index.html"
+  },
+  {
+    "revision": "062ef9643142b5b13ded",
+    "url": "./static/css/main.d39058b3.chunk.css"
+  },
+  {
+    "revision": "b2fa1fe245e6dfc007ef",
+    "url": "./static/js/2.342e67dd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.342e67dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "062ef9643142b5b13ded",
+    "url": "./static/js/main.119e3d87.chunk.js"
+  },
+  {
+    "revision": "befa833bdd9fa631f7c4",
+    "url": "./static/js/runtime-main.c24812da.js"
+  }
+]);
